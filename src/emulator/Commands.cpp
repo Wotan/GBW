344ffe8d167f5c4b46d758dbc956f7df2f47d5f1
Wotan <wotan-notung@hotmail.fr>
2011-12-05 00:05:32 +0100

    Fix load 16 bit HL

--- src/emulator/Commands.cpp
+++ src/emulator/Commands.cpp
@@ -27,19 +27,29 @@ void	Emulator::ToggleIntAfter(bool on)
 
 void	Emulator::Load16bitHL()
 {
-  BYTE tmp = ReadMem(mPC++);
+  SBYTE tmp = (SBYTE)ReadMem(mPC++);
   WORD res;
 
   Z_F = 0;
   N_F = 0;
   H_F = 0;
   C_F = 0;
-  if (((tmp & 0xF) + (mSP & 0xF)) > 0xF)
-    H_F = 1;
-  if (tmp + mSP > 0xFFFF)
-    C_F = 1;
-  res = tmp + mSP;
-  mHL.a = res;
+  if (tmp >= 0)
+    {
+    if (((tmp & 0xF) + (mSP & 0xF)) > 0xF)
+      H_F = 1;
+    if (mSP + tmp > 0xFFFF)
+      C_F = 1;
+    }
+  else
+    {
+      if (((tmp & 0xF) + (mSP & 0xF)) < 0)
+	H_F = 1;
+      if (mSP + tmp < 0)
+	C_F = 1;
+    }
+  res = mSP + tmp;
+  REG_HL = res;
 }
 
 void	Emulator::Push(WORD value)
